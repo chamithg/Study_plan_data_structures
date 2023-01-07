9e14e0d9527efe95c70626a2571f1784c199f800
gamagegit <cgamage92@yahoo.com>
2023-01-07 00:10:37 -0800

    initiated solving 88

---
+++ 88_mearge_sorted_arr.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m):
+            for j in range(n):
+                # TODO
+        
+
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
